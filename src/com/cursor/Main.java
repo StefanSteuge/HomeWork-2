51c93d5d85a2e78080f5df24ea5d06017f1dea37
Stepan Dulka <fotosteuge@gmail.com>
2021-12-20 23:43:55 +0200

    Refactoring

--- src/com/cursor/Main.java
+++ src/com/cursor/Main.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        // 1. Заповнити масив тільки парними числами;
+        //  1. Заповнити масив тільки парними числами;
 
         int[] array = new int[50];
         int i;
@@ -17,7 +17,7 @@ public class Main {
         }
         System.out.println();
 
-        //2. Заповнити масив рандомними числами;
+        //  2. Заповнити масив рандомними числами;
 
         Random random = new Random();
         for (i = 0; i < array.length; i++) {
@@ -26,7 +26,7 @@ public class Main {
         }
         System.out.println();
 
-        //3. Вивести середнє значення масиву;
+        //  3. Вивести середнє значення масиву;
 
         int a;
         int average = 0;
@@ -38,17 +38,17 @@ public class Main {
         System.out.print("Average: " + average / i);
         System.out.println();
 
-        //   4. Вивести суму всіх значень кратних 3;
+        //  4. Вивести суму всіх значень кратних 3;
 
         for (i = 1; i < array.length; i++) {
             array[i] = i;
             if (array[i] % 3 == 0) {
-                System.out.print(array[i] + " ");
+            System.out.print(array[i] + " ");
             }
         }
         System.out.println();
 
-        // 5. Вивести значення всіх парних індексів масиву;
+        //  5. Вивести значення всіх парних індексів масиву;
 
         for (i = 0; i < array.length; i += 2) {
             array[i] = i;
@@ -56,7 +56,7 @@ public class Main {
         }
         System.out.println();
 
-        // 6. Просортувати масив в порядку зростання;
+        //  6. Просортувати масив в порядку зростання;
 
         int[] arraySort = {6, 5, 75, 65, 24, 48, 25, 36, 52, 8};
         for (i = 0; i < arraySort.length; i++) {
@@ -65,7 +65,7 @@ public class Main {
         }
         System.out.println();
 
-        // 7. Вивести найменше значення в масиві;
+        //  7. Вивести найменше значення в масиві;
 
         double[] arrayMin = {5.85, 87.79, 0.78, 8.4, 1.2, 55.89};
         double min = arrayMin[0];
@@ -105,16 +105,19 @@ public class Main {
         System.out.println(array_c[c]);
         System.out.println();
 
- /*       //*   Заповнити масив рандомними числами, вивести їх, потім поміняти місцями парні з непарними;
+/*
+            *   Заповнити масив рандомними числами, вивести їх, потім поміняти місцями парні з непарними;
 
+        int[] array = new int[25];
         Random randomNext = new Random();
         int j = 0;
+        int i;
         for (i = 0; i < array.length; i++) {
-            array[i] = i;
             array[i] = randomNext.nextInt(25);
             System.out.print(array[i] + " ");
         }
         System.out.println();
+
         if (i >= 0 && j >= 0 && i % 2 == 0 && j % 2 != 0) {
             int tmp = j;
             j = array[i];
@@ -126,8 +129,6 @@ public class Main {
                 array[j] = tmp;
             }
         }
-        System.out.print(i + j);
-        */
-
+*/
     }
 }
