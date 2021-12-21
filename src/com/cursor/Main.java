7286f40190c5ad2df8cf715b508e5fa0dcec73c6
Stepan Dulka <fotosteuge@gmail.com>
2021-12-21 18:53:13 +0200

    fix bags 2.0

--- src/com/cursor/Main.java
+++ src/com/cursor/Main.java
@@ -7,27 +7,24 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        //  1. Заповнити масив тільки парними числами;
-
-        int[] array = new int[50];
+        System.out.println("1. Заповнити масив тільки парними числами:");
+        int[] array = new int[25];
         int i;
         for (i = 0; i < array.length; i += 2) {
             array[i] = i;
             System.out.print(array[i] + " ");
         }
-        System.out.println();
 
-        //  2. Заповнити масив рандомними числами;
-
+        System.out.println("\n" + "2. Заповнити масив рандомними числами:");
+        int[] array1 = new int[50];
         Random random = new Random();
-        for (i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(85);
-            System.out.print(array[i] + " ");
+        for (i = 0; i < array1.length; i++) {
+            array1[i] = random.nextInt(85);
+            System.out.print(array1[i] + " ");
         }
-        System.out.println();
 
-        //  3. Вивести середнє значення масиву;
 
+        System.out.println("\n" + "3. Вивести середнє значення масиву:");
         int a;
         int average = 0;
         for (i = 0; i < array.length; i++) {
@@ -36,74 +33,71 @@ public class Main {
             average = average + a;
         }
         System.out.print("Average: " + average / i);
-        System.out.println();
 
-        //  4. Вивести суму всіх значень кратних 3;
 
-        for (i = 1; i < array.length; i++) {
-            array[i] = i;
-            if (array[i] % 3 == 0) {
-            System.out.print(array[i] + " ");
+        System.out.println("\n" + "4. Вивести суму всіх значень кратних 3:");
+        int[] array2 = new int[25];
+        int summ = 0;
+        for (i = 0; i < array2.length; i++) {
+            array2[i] = i;
+            if (i % 3 == 0) {
+                summ += i;
             }
         }
-        System.out.println();
+        System.out.print(summ);
 
-        //  5. Вивести значення всіх парних індексів масиву;
 
+        System.out.println("\n" + "5. Вивести значення всіх парних індексів масиву:");
         for (i = 0; i < array.length; i += 2) {
             array[i] = i;
             System.out.print(i + " ");
         }
-        System.out.println();
 
-        //  6. Просортувати масив в порядку зростання;
 
+        System.out.println("\n" + "6. Просортувати масив в порядку зростання:");
         int[] arraySort = {6, 5, 75, 65, 24, 48, 25, 36, 52, 8};
         for (i = 0; i < arraySort.length; i++) {
             Arrays.sort(arraySort);
             System.out.print(arraySort[i] + " ");
         }
-        System.out.println();
 
-        //  7. Вивести найменше значення в масиві;
 
+        System.out.println("\n" + "7. Вивести найменше значення в масиві:");
         double[] arrayMin = {5.85, 87.79, 0.78, 8.4, 1.2, 55.89};
         double min = arrayMin[0];
         for (i = 0; i < arrayMin.length; i++) {
             if (min > arrayMin[i]) min = arrayMin[i];
         }
-        System.out.print("Мінімальне число в масиві: " + min);
-        System.out.println();
+        System.out.print(min);
 
-        //  8. Вивести найбільше значення масиву;
 
+        System.out.println("\n" + "8. Вивести найбільше значення масиву:");
         double[] arrayMax = {5.85, 87.79, 0.78, 8.4, 1.2, 55.89};
         double max = arrayMax[0];
         for (i = 0; i < arrayMax.length; i++) {
             if (max < arrayMax[i]) max = arrayMax[i];
         }
-        System.out.print("Максимальне число в масиві: " + max);
-        System.out.println();
+        System.out.print(max);
 
-        //  9. Вивести суму значень всіх парних індексів;
 
+        System.out.println("\n" + "9. Вивести суму значень всіх парних індексів:");
         int sum = 0;
         for (i = 0; i < array.length; i += 2) {
             array[i] = i;
             sum += array[i];
         }
-        System.out.print(sum + " ");
-        System.out.println();
+        System.out.print(sum);
 
-        //  10. Вивести перших 20 символів з таблиці ASCI;
 
+        System.out.println("\n"+"10. Вивести перших 20 символів з таблиці ASCI:");
         char[] array_c = new char[20];
         char c;
-        for (c = 0; c < array_c.length - 1; c++) {
-            array_c[c] = c;
+        for (c = 0; c < array_c.length; c++) {
+            array_c[c] = (char)(c);
+            System.out.print(c);
         }
-        System.out.println(array_c[c]);
-        System.out.println();
+       // System.out.print(array_c);g
+
 
 /*
             *   Заповнити масив рандомними числами, вивести їх, потім поміняти місцями парні з непарними;
